b7ef97590cd71021a40c3c595d13d23f8a374156
vocalGoh <98930888+vocalGoh@users.noreply.github.com>
2022-02-15 17:06:52 +0900

    Update MemberDAO.java

--- project/src/main/java/com/example/project/model/member/DAO/MemberDAO.java
+++ project/src/main/java/com/example/project/model/member/DAO/MemberDAO.java
@@ -11,6 +11,11 @@ public interface MemberDAO {
 	public void deleteMember(String userid);//회원정보 삭제
 	public boolean checkPw(String userid, String passwd);//회원정보 수정 패스워드 체크
 	
+	//여기서부터 관리자 단
+	public int countMember(String search_option, String keyword) throws Exception;//레코드 갯수 계산
+	public List<MemberDTO> listMember(String search_option, String keyword, int start, int end);
+	public MemberDTO readMember(String userid) throws Exception;
+	
 	
 	
 	
