8056cc14fb683620eace02b4583f272500413fac
mr05-05 <mr05-05@DESKTOP-2LN3I80>
2022-02-16 09:05:37 +0900

    pager import

--- project/src/main/java/com/example/project/controller/member/MemberController.java
+++ project/src/main/java/com/example/project/controller/member/MemberController.java
@@ -13,14 +13,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.project.model.member.DTO.MemberDTO;
 import com.example.project.service.member.MemberService;
+import com.example.project.service.member.Pager;
 
 
 
