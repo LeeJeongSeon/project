ea3533af17d15cb0c1eef9e27927319291e3c0ed
mr05-05 <mr05-05@DESKTOP-2LN3I80>
2022-02-17 17:41:47 +0900

    update

--- project/src/main/java/com/example/project/controller/event/ReplyController.java
+++ project/src/main/java/com/example/project/controller/event/ReplyController.java
@@ -6,16 +6,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.project.service.event.ReplyService;
 
 @Controller
 @RequestMapping("event_reply/*")
 public class ReplyController {
-
+	
+	@Inject
+	ReplyService replyService;
 	
 	/* 행사페이지로 이동 & 행사리스트 출력 */
 	@RequestMapping("list.do")
 	public ModelAndView list(int er_e_num) throws Exception {
 		ModelAndView mav=new ModelAndView();
+		mav.addObject("list2", replyService.list(er_e_num));
 		mav.setViewName("event/replyList");
 		return mav;
 	}
