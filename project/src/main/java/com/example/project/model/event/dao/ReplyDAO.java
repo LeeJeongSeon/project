123f28b79b5ae12aee9e9edcbf525227ef4550ba
LeeJeongSeon <98930931+LeeJeongSeon@users.noreply.github.com>
2022-02-18 12:39:12 +0900

    Update ReplyDAO.java

--- project/src/main/java/com/example/project/model/event/dao/ReplyDAO.java
+++ project/src/main/java/com/example/project/model/event/dao/ReplyDAO.java
@@ -7,5 +7,8 @@ import com.example.project.model.event.dto.ReplyDTO;
 public interface ReplyDAO {
 
 	public List<ReplyDTO> list(int er_e_num);
+	public ReplyDTO viewDetail(int target);
+	public void update(int er_num, String er_content);
+	public void delete(int er_num);
 
 }
