d5871db5edfa4066afa53288ff6cf6df49386d3c
LeeJeongSeon <98930931+LeeJeongSeon@users.noreply.github.com>
2022-02-18 12:39:31 +0900

    Update ReplyDAOImpl.java

--- project/src/main/java/com/example/project/model/event/dao/ReplyDAOImpl.java
+++ project/src/main/java/com/example/project/model/event/dao/ReplyDAOImpl.java
@@ -1,6 +1,8 @@
 package com.example.project.model.event.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -18,6 +20,24 @@ public class ReplyDAOImpl implements ReplyDAO {
 	@Override
 	public List<ReplyDTO> list(int er_e_num) {
 		return sqlSession.selectList("reply.list", er_e_num);
+	}
+
+	@Override
+	public ReplyDTO viewDetail(int target) {
+		return sqlSession.selectOne("reply.viewDetail", target);
+	}
+
+	@Override
+	public void update(int er_num, String er_content) {
+		Map<String, Object> map=new HashMap<>();
+		map.put("er_num", er_num);
+		map.put("er_content", er_content);
+		sqlSession.update("reply.update", map);
+	}
+
+	@Override
+	public void delete(int er_num) {
+		sqlSession.delete("reply.delete", er_num);
 	}
 
 }
