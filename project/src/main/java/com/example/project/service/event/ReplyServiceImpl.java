d8836be14042dce60c041019c3d2501b673698c5
LeeJeongSeon <98930931+LeeJeongSeon@users.noreply.github.com>
2022-02-18 12:41:28 +0900

    Update ReplyServiceImpl.java

--- project/src/main/java/com/example/project/service/event/ReplyServiceImpl.java
+++ project/src/main/java/com/example/project/service/event/ReplyServiceImpl.java
@@ -20,4 +20,19 @@ public class ReplyServiceImpl implements ReplyService {
 		return replyDao.list(er_e_num);
 	}
 
+	@Override
+	public ReplyDTO viewDetail(int target) throws Exception {
+		return replyDao.viewDetail(target);
+	}
+
+	@Override
+	public void update(int er_num, String er_content) throws Exception {
+		replyDao.update(er_num, er_content);
+	}
+
+	@Override
+	public void delete(int er_num) throws Exception {
+		replyDao.delete(er_num);
+	}
+
 }
