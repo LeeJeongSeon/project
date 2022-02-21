e1c76167b541bdf92397410c43657bb2e36cd42e
nigan78 <98930954+nigan78@users.noreply.github.com>
2022-02-21 17:39:58 +0900

    Update RentDAO.java

--- project/src/main/java/com/example/project/model/rent/DAO/RentDAO.java
+++ project/src/main/java/com/example/project/model/rent/DAO/RentDAO.java
@@ -6,10 +6,13 @@ import com.example.project.model.rent.DTO.RentDTO;
 
 public interface RentDAO {
 	public List<RentDTO> listRent(String userid);
+	public List<RentDTO> listRentN(String userid);
 	public void insert(RentDTO dto);
 	public void delete(int bnum);
+	public void re(int bnum);
 	public void update(int bnum);
 	public int countRent(String userid, String book_id);
 	public void modifyRent(RentDTO dto);
 	public void extend(int bnum);
+	public void show(int bnum);
 }
