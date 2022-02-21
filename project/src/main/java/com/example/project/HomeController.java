b27fe3df44f7ad4be306d5934201e9f12bb1d9de
mr05-07 <mr05-07@DESKTOP-I462UFO>
2022-02-21 17:30:17 +0900

    home adminCk update

--- project/src/main/java/com/example/project/HomeController.java
+++ project/src/main/java/com/example/project/HomeController.java
@@ -4,6 +4,9 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -11,28 +14,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.project.service.member.MemberService;
+
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
+	@Inject
+	MemberService memberService;
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model ,HttpSession session) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+		int adminCk=0;
+		String id=(String)session.getAttribute("userid");
+		if(id != null) {
+		 adminCk = memberService.adminCheck(id);
+		}
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		model.addAttribute("adminCk", adminCk);
 		return "home";
 	}
 	
